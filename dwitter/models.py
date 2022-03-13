a2e0bfab3f5e0f640652e838f2a66545ffec7402
Ayikanying <Ayikanying@users.noreply.github.com>
2022-03-13 14:10:36 +0300

    front end with bulma css

--- dwitter/models.py
+++ dwitter/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
@@ -11,10 +11,11 @@ class Profile(models.Model):
     def __str__(self) -> str:
         return self.user.username
 
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
         user_profile.save()
-        user_profile.follows.set([instance])
+        user_profile.follows.set([instance.profile.id])
         user_profile.save()
-post_save.connect(create_profile, sender=User)
+#post_save.connect(create_profile, sender=User)
